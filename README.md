c52c574381cc9e3e17112245b0ffb799794daba2
TheSongList <17340748+TheSongList@users.noreply.github.com>
2019-05-17 09:14:38 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # rand-string
 Generates a random string of a specified length.
+
+# Usage
+Currently there are 4 types:
+- uppercase
+- lowercase
+- upperlower
+- alphanumerical 
+
+You can probably guess what each one would create
+
+The function requires 2 parameters type and length; Type of course is the above.
+```py
+RandString(type, length)
+```
+Now lets look at some examples
+
+uppercase:
+```py
+print(RandString("uppercase", 10))
+# Output: TPDROEAVRY
+```
+
+lowercase:
+```py
+print(RandString("lowercase", 10))
+# Output: mdvsewzkwf
+```
+
+upperlower:
+```py
+print(RandString("upperlower", 10))
+# Output: YjHFGzysFx
+```
+
+alphanumerical:
+```py
+print(RandString("alphanumerical", 10))
+# Output: Fy0Nbqno6B
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = "rand-string",
+  packages = ["rand-string"],
+  version = "0.1",
+  license="MIT",
+  description = "Generates a random string of a specified length.",
+  author = "TheSongList",
+  author_email = "the@songlist.moe",
+  url = "https://github.com/TheSongList/rand-string",
+  download_url = "https://github.com/TheSongList/rand-string/archive/v0.1.tar.gz",
+  keywords = ["RANDOM", "STRING", "GENERATOR"],
+  install_requires=[
+      ],
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+
+    "ntended Audience :: Developers",
+    "Topic :: Software Development :: Build Tools",
+
+    "License :: OSI Approved :: MIT License",
+
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+  ],
+)
